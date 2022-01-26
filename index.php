3ec30b8213a42ffbe2faadca85569cee319d0fbb
SamuelLourens03 <samuellaurens9@gmail.com>
2022-01-26 14:43:59 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,5 +10,10 @@
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia suscipit repellendus reiciendis minus voluptates. Excepturi voluptatem debitis incidunt quidem repellendus, accusamus omnis eius alias accusantium repellat id! Laborum, eos a!   </p>
     
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+
 </body>
 </html>
